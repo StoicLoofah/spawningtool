ea40050841fdb7e3db6e89cdc0960759fd770ef5
Kevin Leung <kkleung89@gmail.com>
2013-05-08 04:21:36 -0700

    tweaks to make this a usable module

--- spawningtool.py
+++ spawningtool.py
@@ -1,11 +1,15 @@
+import argparse
+
 from s2protocol import protocol15405
 from s2protocol.mpyq import mpyq
 
 from constants import *
 
+
 def _gameloop_to_time(gameloop):
     seconds = gameloop / 16
     return '{0}:{1:02d}'.format(seconds / 60, seconds % 60)
+
 
 class TrackerEvent(object):
     def __init__(self, gameloop, supply=None):
@@ -41,23 +45,6 @@ class GameTimeline(object):
         return '\n'.join([unicode(event) for event in self.timeline])
 
 
-archive = mpyq.MPQArchive('replays/myreplay.SC2Replay')
-
-contents = archive.header['user_data_header']['content']
-header = protocol15405.decode_replay_header(contents)
-
-baseBuild = header['m_version']['m_baseBuild']
-try:
-    protocol_name = 'protocol%s' % (baseBuild,)
-    _temp = __import__('s2protocol', globals(), locals(), [protocol_name])
-    protocol = getattr(_temp, protocol_name)
-except:
-    print >> sys.stderr, 'Unsupported base build: %d' % baseBuild
-    sys.exit(1)
-
-builds = [GameTimeline() for i in range(2)]
-supplies = [[[0, 6]] for i in range(2)]
-
 def get_supply(supply, gameloop):
     start = 0
     end = len(supply)
@@ -78,45 +65,77 @@ def get_supply(supply, gameloop):
     return supply[start][1]
 
 
-if hasattr(protocol, 'decode_replay_tracker_events'):
-    contents = archive.read_file('replay.tracker.events')
+def parse_replay(file_name):
+    archive = mpyq.MPQArchive(file_name)
 
-    for event in protocol.decode_replay_tracker_events(contents):
-        event_type = event['_event']
-        if event['_gameloop'] == 0:
-            continue
-        if event_type == 'NNet.Replay.Tracker.SPlayerStatsEvent':
-            supplies[event['m_playerId'] - 1].append(
-                    [event['_gameloop'], event['m_stats']['m_scoreValueFoodUsed'] / 4096])
-        elif event_type == 'NNet.Replay.Tracker.SUnitBornEvent':  # need to reverse this
-            player = event['m_controlPlayerId']
-            unit_name = event['m_unitTypeName']
-            if unit_name in BO_EXCLUDED or player == 0:
-                continue
-            gameloop = event['_gameloop'] - BUILD_TIMES[unit_name]
-            supply = get_supply(supplies[player - 1], gameloop)
-            builds[player - 1].add_event(BuildEvent(unit_name, gameloop, supply))
-        elif event_type == 'NNet.Replay.Tracker.SUnitInitEvent':
-            player = event['m_controlPlayerId']
-            unit_name = event['m_unitTypeName']
-            if unit_name in BO_EXCLUDED or player == 0:
-                continue
-            gameloop = event['_gameloop']
-            supply = supplies[player - 1][-1][1]
-            builds[player - 1].add_event(BuildEvent(unit_name, gameloop, supply))
-        elif event_type == 'NNet.Replay.Tracker.SUpgradeEvent':  # need to reverse this
-            player = event['m_playerId']
-            if player == 0:
-                continue
-            unit_name = event['m_upgradeTypeName']
-            gameloop = event['_gameloop'] - BUILD_TIMES[unit_name]
-            supply = get_supply(supplies[player - 1], gameloop)
-            builds[player - 1].add_event(BuildEvent(unit_name, gameloop, supply))
+    contents = archive.header['user_data_header']['content']
+    header = protocol15405.decode_replay_header(contents)
 
-    print supplies
-    for build in builds:
-        build.sort()
-        print unicode(build)
-else:
-    print 'cannot do anything with this'
+    baseBuild = header['m_version']['m_baseBuild']
+    try:
+        protocol_name = 'protocol%s' % (baseBuild,)
+        _temp = __import__('s2protocol', globals(), locals(), [protocol_name])
+        protocol = getattr(_temp, protocol_name)
+    except:
+        return False
+
+    builds = [GameTimeline() for i in range(2)]
+    supplies = [[[0, 6]] for i in range(2)]
+
+    if hasattr(protocol, 'decode_replay_tracker_events'):
+        contents = archive.read_file('replay.tracker.events')
+
+        for event in protocol.decode_replay_tracker_events(contents):
+            event_type = event['_event']
+            if event['_gameloop'] == 0:
+                continue
+            if event_type == 'NNet.Replay.Tracker.SPlayerStatsEvent':
+                supplies[event['m_playerId'] - 1].append(
+                        [event['_gameloop'], event['m_stats']['m_scoreValueFoodUsed'] / 4096])
+            elif event_type == 'NNet.Replay.Tracker.SUnitBornEvent':  # need to reverse this
+                player = event['m_controlPlayerId']
+                unit_name = event['m_unitTypeName']
+                if unit_name in BO_EXCLUDED or player == 0:
+                    continue
+                gameloop = event['_gameloop'] - BUILD_TIMES[unit_name]
+                supply = get_supply(supplies[player - 1], gameloop)
+                builds[player - 1].add_event(BuildEvent(unit_name, gameloop, supply))
+            elif event_type == 'NNet.Replay.Tracker.SUnitInitEvent':
+                player = event['m_controlPlayerId']
+                unit_name = event['m_unitTypeName']
+                if unit_name in BO_EXCLUDED or player == 0:
+                    continue
+                gameloop = event['_gameloop']
+                supply = supplies[player - 1][-1][1]
+                builds[player - 1].add_event(BuildEvent(unit_name, gameloop, supply))
+            elif event_type == 'NNet.Replay.Tracker.SUpgradeEvent':  # need to reverse this
+                player = event['m_playerId']
+                if player == 0:
+                    continue
+                unit_name = event['m_upgradeTypeName']
+                gameloop = event['_gameloop'] - BUILD_TIMES[unit_name]
+                supply = get_supply(supplies[player - 1], gameloop)
+                builds[player - 1].add_event(BuildEvent(unit_name, gameloop, supply))
+
+        for build in builds:
+            build.sort()
+
+        return [unicode(build) for build in builds]
+
+    return False
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('replay_file', help='.SC2Replay file to load')
+    args = parser.parse_args()
+    ret = parse_replay(args.replay_file)
+
+    if ret:
+        for build in ret:
+            print ret[0]
+            print ''
+    else:
+        print 'Unable to parse replays'
+
 
