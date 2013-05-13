c681284ae794703efbc01786319d382bc74a1397
Kevin Leung <kkleung89@gmail.com>
2013-05-12 22:24:19 -0700

    does not support 2.6 anymore because sc2reader uses OrderedDict, which is only available in collections in 2.7

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
         "Natural Language :: English",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Topic :: Games/Entertainment",
         "Topic :: Games/Entertainment :: Real Time Strategy",
